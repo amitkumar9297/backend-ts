9f29b8044c7404edec5230e4464b24a1a99e75d1
Amit Kumar <amitkumar790194@gmail.com>
2025-02-04 01:26:13 +0530

    swagger api/docs enable

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 import swaggerUi from "swagger-ui-express";
 import userRoutes from "./user/user.routes";
-// import swaggerJsonFile from "../swagger/swagger.json"
+import swaggerJsonFile from "../swagger/swagger.json"
 // import { isAdminMiddleware } from "./common/middleware/isAdmin.middleware";
 
 
@@ -11,6 +11,6 @@ const router = express.Router();
 
 router.use("/users" ,userRoutes);
 
-// router.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerJsonFile));
+router.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerJsonFile));
 
 export default router;
